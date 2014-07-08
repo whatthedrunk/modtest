87a7dd2e458d5d5b8b693a884956ac42321d0e0b
whatthedrunk <mattmaryanski@gmail.com>
2014-07-08 13:53:56 -0700

    first commit

---
+++ src/main/java/com/whatthedrunk/toomuchwood/toomuchwood.java
@@ -0,0 +1,34 @@
+package com.whatthedrunk.toomuchwood;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="toomuchwood", name="Too Much Wood", version="1.7.10-1.0")
+
+public class toomuchwood
+{
+    @Mod.Instance("toomuchwood")
+    public static toomuchwood instance;
+
+
+    @Mod.EventHandler
+    public void preint(FMLPreInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void preint(FMLInitializationEvent event)
+    {
+
+    }
+    @Mod.EventHandler
+    public void preint(FMLPostInitializationEvent event)
+    {
+
+    }
+
+
+
+}
